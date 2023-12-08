0915743ae36dea6ff9315b5c442c9b02cacd03fd
lunahilly <35861@ma-web.nl>
2023-12-08 11:43:43 +0100

    .

---
+++ code/foto/photo.js
@@ -0,0 +1,42 @@
+const width = 640, height = 480,
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext('2d');
+
+let video = null, isSetup = null;
+
+SetUp();
+function SetUp(){
+    if(!isSetup){
+        canvas.width = width;
+        canvas.height = height;
+        canvas.style.width = `${width}px`;
+        canvas.style.width = `${width}px`;
+        canvas.style.backgroundColor = "red";
+
+        video = document.getElementById("camera");
+        document.addEventListener('click', takePhoto);
+
+        navigator.mediaDevices
+        .getUserMedia({video: true, audio: false})
+        .then((stream) =>{
+            video.srcObject = stream;
+            video.play();
+        }).catch((err)=>{
+            console.error(`Error in obtaining video stream :\n ${err}`);
+        });
+
+
+        isSetup != isSetup;
+    }
+}
+
+function takePhoto(e){
+    e.preventDefault();
+
+    ctx.drawImage(video, 0, 0, width, height);
+}
+
+
+//npx eslint init
+
+//load cells voor stoel op zitten en muziek
