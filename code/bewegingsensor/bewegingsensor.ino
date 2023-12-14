8f6c2f3bd6dd00c68ac39c88565ca050e71cb994
lunahilly <35861@ma-web.nl>
2023-12-14 15:30:41 +0100

    onderzoekverslag van de beweging sensor
    code doet het en is getest

---
+++ code/bewegingsensor/bewegingsensor.ino
@@ -0,0 +1,32 @@
+//deze code zorgt ervoor dat als iemand langs de beweging sensor gaat, de led lampjes 1 voor 1 gaan branden.
+
+
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(13, INPUT); //beweging sensor
+  pinMode(2, OUTPUT); //led 1
+  pinMode(3, OUTPUT); //led 2
+  pinMode(4, OUTPUT); //led 3
+}
+
+void loop() {
+  if(digitalRead(13)==HIGH) {
+    Serial.println("Movement detected.");
+    digitalWrite(2, HIGH);  
+        delay(1000);  
+    digitalWrite(3, HIGH);  
+        delay(1000);
+    digitalWrite(4, HIGH); 
+    delay(3000);
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    delay(3000);
+
+  } else {
+    Serial.println("Did not detect movement.");
+      delay(3000);
+  }
+}
