8405bb0a09d19cc9976e2a99095a9a16384d82e8
lunahilly <35861@ma-web.nl>
2024-01-21 23:36:09 +0100

    logboek update

--- SD/code/bewegingsensor/bewegingsensor.ino
+++ SD/code/bewegingsensor/bewegingsensor.ino
@@ -10,6 +10,7 @@ void setup()
   pinMode(2, OUTPUT); //led 1
   pinMode(3, OUTPUT); //led 2
   pinMode(4, OUTPUT); //led 3
+  pinMode(5, OUTPUT); //led 4
 }
 
 void loop() {
@@ -20,7 +21,7 @@ void loop() {
     digitalWrite(3, HIGH);  
         delay(1000);
     digitalWrite(4, HIGH); 
-    delay(3000);
+    delay(1000);
     digitalWrite(5, HIGH); 
     delay(3000);
     digitalWrite(2,LOW);
